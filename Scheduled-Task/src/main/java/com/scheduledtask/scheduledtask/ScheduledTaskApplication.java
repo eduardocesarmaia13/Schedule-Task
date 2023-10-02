a9a187b2f316f967c3ef6aa0471e82ae8b5109ce
Eduardo Cesar Maia Moreira Oliveira <eduardomaia@ibm.com>
2023-10-02 15:24:25 -0300

    Primeiro Commit

---
+++ Scheduled-Task/src/main/java/com/scheduledtask/scheduledtask/ScheduledTaskApplication.java
@@ -0,0 +1,15 @@
+package com.scheduledtask.scheduledtask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+public class ScheduledTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScheduledTaskApplication.class, args);
+	}
+
+}
