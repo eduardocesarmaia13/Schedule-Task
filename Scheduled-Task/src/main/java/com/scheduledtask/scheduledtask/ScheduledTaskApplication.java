cd7f7f2e9f3064fabad5101eb96e9739f1ca4bb4
Eduardo Cesar Maia Moreira Oliveira <eduardomaia@ibm.com>
2023-10-02 15:54:01 -0300

    FixedRate

--- Scheduled-Task/src/main/java/com/scheduledtask/scheduledtask/ScheduledTaskApplication.java
+++ Scheduled-Task/src/main/java/com/scheduledtask/scheduledtask/ScheduledTaskApplication.java
@@ -1,15 +1,28 @@
 package com.scheduledtask.scheduledtask;
 
+import org.apache.logging.log4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+
+import java.util.Date;
 
 @SpringBootApplication
 @EnableScheduling
 public class ScheduledTaskApplication {
 
+	Logger logger = (Logger) LoggerFactory.getLogger(ScheduledTaskApplication.class);
+
 	public static void main(String[] args) {
+
 		SpringApplication.run(ScheduledTaskApplication.class, args);
 	}
 
+	@Scheduled(fixedRate = 2000L)
+	public void job() throws InterruptedException {
+		logger.info("Job Current Time" + new Date());
+		Thread.sleep(1000L);
+	}
 }
